76405ac476eb3a3af7bc1433dc496485c4b9bea1
Diego Correa <diego.correa.br@AC-LPT-00829.local>
2022-05-31 12:52:25 -0300

    add mocha

---
+++ test/application.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('Example Tests', () => { // the tests container
+    it('checking 1 == 1', () => { // the single test
+        expect(1 === 1)
+    });
+});
