d108251821b042cacda3e2ff8799759671e86fc6
Plinio Duarte <plinio.junior@avenuecode.com>
2022-06-01 14:27:31 -0300

    chore: add jest and remove mocha

--- test/unit/application.test.ts
+++ test/unit/application.test.ts
@@ -1,7 +1,6 @@
-import { expect } from 'chai';
-
 describe('Example Tests', () => { // the tests container
     it('checking 1 == 1', () => { // the single test
+        console.log('unit test')
         expect(1 === 1)
     });
 });
