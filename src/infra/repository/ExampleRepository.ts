43501322f8efc2602289dbe81fd2ba2f82026f27
Plinio Duarte <plinio.junior@avenuecode.com>
2022-06-07 14:30:55 -0300

    chore: add eslint rules

--- src/infra/repository/ExampleRepository.ts
+++ src/infra/repository/ExampleRepository.ts
@@ -1,9 +1,7 @@
-import { BaseRepository } from "./base/BaseRepository";
-import { ExampleModel } from "../../domain/model/ExampleModel";
-
+import { BaseRepository } from './base/BaseRepository'
+import { ExampleModel } from '../../domain/model/ExampleModel'
 
 export class ExampleRepository extends BaseRepository {
-
-  public getAll = async (): Promise<Array<ExampleModel>> =>
-    await this.repository.example.findMany();
+  public getAll = async (): Promise<ExampleModel[]> =>
+    this.repository.example.findMany()
 }
