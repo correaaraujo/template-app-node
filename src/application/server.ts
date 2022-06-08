1e03cf2d0e99fab87d512ec1b951f9eb7955ff04
Plinio Duarte <plinio.junior@avenuecode.com>
2022-06-08 17:40:01 -0300

    refactor: remove duplicated imports

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,96 +1,74 @@
 
 import express from 'express'
+import 'dotenv/config'
 import { Server as HttpServer } from 'node:http'
 import swaggerUi from 'swagger-ui-express'
-import 'dotenv/config'
 import { container } from 'tsyringe'
 import * as swaggerDoc from '../../swagger.json'
 import Logger from '@infra/Logger/Logger'
 import ExitStatus from '@shared/enum/ExitStatus'
 import HttpLogger from '@infra/HttpLogger/HttpLogger'
-import HealthCheckController from "./controllers/HealthcheckController";
-import "dotenv/config";
-import morgan from "morgan";
-import ExampleController from "./controllers/ExampleController";
-import { inject, injectable } from "tsyringe";
+import HealthCheckController from './controllers/HealthcheckController'
+import ExampleController from './controllers/ExampleController'
+const logger = container.resolve(Logger)
 
-@injectable()
 export default class Server {
-    app: express.Application;
-    readonly logger = container.resolve(Logger)
+  app: express.Application
 
-    constructor() {
-        this.app = express();
-        this.setupHttpLogger()
-        this.setupRoutes()
-        this.setupUnhandledOperations()
-    }
+  constructor () {
+    this.app = express()
+    this.setupHttpLogger()
+    this.setupRoutes()
+    this.setupUnhandledOperations()
+  }
 
+  private readonly setupHttpLogger = (): void => {
+    this.app.use(HttpLogger)
+  }
 
+  private readonly setupRoutes = (): void => {
+    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+    this.app.use('/api/v1/healthcheck', new HealthCheckController().router)
+    this.app.use('/api/v1/examples', new ExampleController().router)
+  }
 
-    private readonly setupHttpLogger = (): void => {
-        this.app.use(HttpLogger)
-    }
+  /**
+   * Handles unhandledRejection and uncaughtException events.
+   */
+  private readonly setupUnhandledOperations = (): void => {
+    process.on('unhandledRejection', (reason, promise) => {
+      logger.error(`App exiting due to an unhandled promise: ${String(promise)} and reason ${String(reason)}`)
+      throw reason
+    })
+    process.on('uncaughtException', (error) => {
+      logger.error(`App exiting due to an uncaught error: ${String(error)}`)
+      process.exit(ExitStatus.FAILURE)
+    })
+  }
 
-    setupApplicationLogger = () => {
-        this.app.use(morgan(function (tokens, req, res) {
-            const msg = [
-                tokens.method(req, res),
-                tokens.url(req, res),
-                tokens.status(req, res),
-                tokens.res(req, res, 'content-length'), '-',
-                tokens['response-time'](req, res), 'ms',
-            ].join(' ');
-            this.logger.http(msg);
-            return null;
-            // return msg;
-        })
-        );
-    }
-
-    private readonly setupRoutes = (): void => {
-        this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
-        this.app.use('/api/v1/healthcheck', new HealthCheckController().router)
-        this.app.use('/api/v1/examples', new ExampleController().router)
-    }
-
-    /**
-     * Handles unhandledRejection and uncaughtException events.
-     */
-    private readonly setupUnhandledOperations = (): void => {
-        process.on('unhandledRejection', (reason, promise) => {
-            this.logger.error(`App exiting due to an unhandled promise: ${String(promise)} and reason ${String(reason)}`)
-            throw reason
-        })
-        process.on('uncaughtException', (error) => {
-            this.logger.error(`App exiting due to an uncaught error: ${String(error)}`)
+  /**
+   * Configures a gracefull shutdown whenever the server crash
+   * @param server - The server instance.
+   */
+  static readonly setupGracefulShutdown = (server: HttpServer): void => {
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+    exitSignals.forEach((signal: string) => {
+      process.on(signal, () => {
+        // logger.info(`Closing server connections due an ${signal} command...`)
+        // TODO: Close DB and other connections
+        server.close((serverError) => {
+          if (serverError) {
+            // logger.error('An unexpected error ocurred while trying to shut down...')
+            // logger.error(String(serverError))
             process.exit(ExitStatus.FAILURE)
+          } else {
+            // logger.info('Server connections closed successfully!!!')
+            process.exit(ExitStatus.SUCCESS)
+          }
         })
-    }
+      })
+    })
+  }
 
-    /**
-     * Configures a gracefull shutdown whenever the server crash
-     * @param server - The server instance.
-     */
-    static readonly setupGracefulShutdown = (server: HttpServer): void => {
-        const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
-        exitSignals.forEach((signal: string) => {
-            process.on(signal, () => {
-                //logger.info(`Closing server connections due an ${signal} command...`)
-                // TODO: Close DB and other connections
-                server.close((serverError) => {
-                    if (serverError) {
-                        //logger.error('An unexpected error ocurred while trying to shut down...')
-                        //logger.error(String(serverError))
-                        process.exit(ExitStatus.FAILURE)
-                    } else {
-                        //logger.info('Server connections closed successfully!!!')
-                        process.exit(ExitStatus.SUCCESS)
-                    }
-                })
-            })
-        })
-    }
-
-    static bootstrap = (): Server => new Server()
+  static bootstrap = (): Server => new Server()
 }
--- src/entry.ts
+++ src/entry.ts
@@ -3,8 +3,6 @@ import 'reflect-metadata'
 import './shared/container/RegisterDependencyInjections'
 import { Server as HttpServer } from 'node:http'
 import { container } from 'tsyringe'
-import "reflect-metadata"
-
 
 import Server from '@application/server'
 import Logger from '@infra/Logger/Logger'
@@ -13,13 +11,13 @@ const port = process.env.PORT
 const logger = container.resolve(Logger)
 
 void (async (): Promise<void> => {
-    try {
-        const server: HttpServer = Server.bootstrap().app.listen(port, () => {
-            logger.info(`Server running on port ${port}!`)
-        })
-        Server.setupGracefulShutdown(server)
-    } catch (error) {
-        logger.error('An unexpected error prevented the server from starting up.')
-        logger.error(String(error))
-    }
+  try {
+    const server: HttpServer = Server.bootstrap().app.listen(port, () => {
+      logger.info(`Server running on port ${port}!`)
+    })
+    Server.setupGracefulShutdown(server)
+  } catch (error) {
+    logger.error('An unexpected error prevented the server from starting up.')
+    logger.error(String(error))
+  }
 })()
